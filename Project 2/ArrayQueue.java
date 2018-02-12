bc9196d8899750278e7fbd2a8311c49313f48e9e
Hannah Riedman <hannah.riedman1@marist.edu>
2018-02-12 15:59:20 -0500

    Adding provided code for ArrayQueue

---
+++ Project 2/ArrayQueue.java
@@ -0,0 +1,44 @@
+/* CMPT 435
+ * Project 2 - Shortest-path word-melt solver
+ * Filename: ArrayQueue.java
+ * Student name: Hannah Riedman
+ *
+ * ArrayQueue class for the Word-Melt Solver.
+ * Modifed code from Professor Rivas.
+ */
+
+class ArrayQueue {
+  private Location[] data;
+  private int length, capacity, front;
+
+  private void doubleCapacity() {
+    // -
+  }
+
+  ArrayQueue() {
+    // -
+  }
+  ArrayQueue(ArrayQueue q) {
+    // -
+  }
+
+  void add(Location loc) {
+    // -
+  }
+
+  void remove() {
+    // -
+  }
+
+  Location getFront() {
+    // -
+  }
+
+  int getLength()  {
+    // -
+  }
+
+  ArrayQueue copyFrom(ArrayQueue q) {
+  // -
+  }
+}
