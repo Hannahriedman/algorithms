e759994c3a5b647a4721a6666a114f42591da30f
Hannah Riedman <hannah.riedman1@marist.edu>
2018-02-18 17:52:19 -0500

    Editing comments and fixing style

--- Project 1/Driver_prj1.java
+++ Project 1/Driver_prj1.java
@@ -5,92 +5,84 @@
  * 1-31-18
  * Final Driver for the Maze Solver project
  */
-
 import java.util.Scanner;
 
 public class Driver_prj1 {
+
   /*
    * main
    *  parameters:
    *      args -- the array of command line argument values
    *  return value: nothing
-   *  
+   *
    *  In this main function, we will take in a file input, a test maze,
    *  and get the number of valid locations, cordinates of valid locations,
    *  and the start and end locations. This is done using the streamIn method
-   *  in the Maze class. Then we will inialize the first location and push it 
+   *  in the Maze class. Then we will inialize the first location and push it
    *  to the stack. The While loop will run through the maze until either the
    *  stack is empty and the location is done OR the end location is found.
    *  Depending on the while loop outcome, the last if statement will print
    *  Soultion Found or No Soultion Found.
 	 */
-
   public static void main(String[] args) {
 	  Scanner file = new Scanner(System.in);
-	  
+
 	  // inialize variables
 	  String valid = "no";
 	  boolean flag = true;
-	  
-	  // initalize Location, Maze and Stack 
+
+	  // initalize Location, Maze and Stack
 	  Maze newMaze = new Maze();
 	  LocationStack stack = new LocationStack();
 	  Location loc = new Location();
 
 	  // stream in the file
 	  newMaze.streamIn(file);
-	  
+
 	  loc = newMaze.getStartLocation(); // start location
 	  stack.push(loc); // push start to stack
 	  loc.start(); // inialize the first direction
 
 	  // while the flag is true, it will run through the logic
 	  while(flag) {
-		
-        if(loc.isDone() && stack.isEmpty()) { // if there is no solution
-          flag = false;
-          valid = "no";
-        } else if (newMaze.isEndLocation(loc)){ // if a solution is found
-          flag = false;
-          valid = "Valid";
-        } else { // if the location is not done or the end location,
+		  if(loc.isDone() && stack.isEmpty()) { // if there is no solution
+        flag = false;
+        valid = "no";
+      } else if (newMaze.isEndLocation(loc)){ // if a solution is found
+        flag = false;
+        valid = "Valid";
+      } else { // if the location is not done or the end location,
         // and the stack still has locations, continue.
-          Location neighbor = loc.nextNeighbor(); // create a neighbor location 
-         
-          if (newMaze.isValidLocation(neighbor) && !(stack.isOn(neighbor))) {
-        	// if neighbor is a valid location and is not on stack then push it
-            stack.push(neighbor);
-            if (newMaze.isEndLocation(neighbor)) { 
-            // if its the end location then break out of loop
-              valid = "Valid"; // soltuion found 
-              flag = false;
-            } else {
-              loc = neighbor;
-              loc.start();
-            } 
-          } else if (loc.isDone()) {  // if location is done 
-
-        	stack.pop(); // take it off the stack
-        	if (stack.isEmpty()) { 
-        	// if the stack is empty then break out of loop 
-              flag = false; 
-              valid = "no"; // no soltuion found
-        	} else {
-              // set the new top of stack to the location.
-              loc = stack.getTop(); 
-            }
+        Location neighbor = loc.nextNeighbor(); // create a neighbor location
+        if (newMaze.isValidLocation(neighbor) && !(stack.isOn(neighbor))) {
+        // if neighbor is a valid location and is not on stack then push it
+          stack.push(neighbor);
+          if (newMaze.isEndLocation(neighbor)) {
+          // if its the end location then break out of loop
+            valid = "Valid"; // soltuion found
+            flag = false;
+          } else {
+            loc = neighbor;
+            loc.start();
+          }
+        } else if (loc.isDone()) {  // if location is done
+          stack.pop(); // take it off the stack
+        	if (stack.isEmpty()) { // if the stack is empty then break out of loop
+            flag = false;
+            valid = "no"; // no soltuion found
+        	} else { // set the new top of stack to the location.
+            loc = stack.getTop();
           }
         }
+      }
 	  }
-	  
+
 	  // checks if soltuion was found and prints accurate message.
 	  if (valid.equals("Valid")) {
 	    System.out.println("Solution found:");
 	    stack.streamOut(stack);
 	  } else {
-		System.out.println("No solution found"); 
+		  System.out.println("No solution found");
 	  }
-
   }
 }
-
--- Project 1/Maze.java
+++ Project 1/Maze.java
@@ -5,43 +5,12 @@
  *
  * Maze class for the Maze Solver
  */
-
 import java.util.Scanner;
 
-/* A Maze object contains three things: the starting location, the
- * ending location, and a list of valid locations. It does not contain
- * any logic for actually solving the maze.
- *
- * The Maze constructor initializes all the data members that it can,
- * but the validLocationCount will not yet be known when the Maze is
- * constructed, so use a sensible value here.
- *
- * getStartLocation() returns the starting location of the maze.
- * isEndLocation() returns true if the given Location is the end of
- * the maze, otherwise false.  isValidLocation() returns true if the
- * given Location is in the list of valid Locations, otherwise false.
- *
- * streamIn() provides a means of streaming in a Maze object.  In
- * this version, streamIn() does not do any error checking.  We
- * assume that the entered data is perfect. Consider the following
- * when writing this function: what if we stream in a Maze object with
- * this function, and later call the same function on the same object?
- *
- * validLocationCount keeps the number of valid locations, which will
- * not be known until the object is streamed in. This is the length of
- * the validLocations array.
- *
- * validLocations is an array that contains all the locations that may
- * be used to solve the maze.
- *
- * startLocation holds the starting location of the maze, and
- * endLocation holds the target location of the maze.
- *
- * In this class we make the copy constructor private and illegal to
- * call, because it is not needed in this project, and we don't
- * want the compiler to provide them for us (since this class uses
- * dynamically allocated memory).  If these methods are called, they
- * will intentionally crash the program by the call to assert(false).
+/**
+ * The Maze class will hold all the attrubutes of a Maze. Most information
+ * will be taken from an input file and the streamIn method will organize
+ * this data into validLocations, endLocation, and the startLocation.
  */
 class Maze {
   private Maze(Maze m) { assert(false); }
@@ -68,10 +37,10 @@ class Maze {
   /**
    * isValidLoation method
    * @param loc location that will check if its a valid location
-   * @return boolean will be true if location is valid 
+   * @return boolean will be true if location is valid
    */
   boolean isValidLocation(Location loc) {
-	boolean valid = false; // boolean that returns result
+	  boolean valid = false; // boolean that returns result
     for (int i=0; i < validLocations.length;i++) {
       if (loc.isEqual(validLocations[i])) {
         valid= true;
@@ -88,35 +57,35 @@ class Maze {
     if (loc.isEqual(endLocation)) {
       return true;
     } else {
-      return false; 
+      return false;
     }
   }
   /**
    * StreamIn method
-   * This will intake Scanner input and set the first int to 
-   * the number of valid locations. Then it will loop through 
+   * This will intake Scanner input and set the first int to
+   * the number of valid locations. Then it will loop through
    * the validlocations and add them into an array of locations.
-   * Finally it will add the last two cordinates into the start 
+   * Finally it will add the last two cordinates into the start
    * and end location variables respectively.
-   * @param input from the Scanner 
+   * @param input from the Scanner
    */
   void streamIn(Scanner input) {
     validLocationCount = input.nextInt();
     validLocations = new Location[validLocationCount];
-    
+
     for (int i =0; i <= validLocationCount;i++) {
       // adds all the valid loactions to the array
       if (i < validLocationCount) {
         Location loc = new Location();
-    	loc.streamIn(input);
-        validLocations[i] = loc; 
-      } else { 
-    	startLocation = new Location();
-    	endLocation = new Location();
-    	  
-    	startLocation.streamIn(input);
-    	endLocation.streamIn(input);
-      }      
-    } 
+    	  loc.streamIn(input);
+        validLocations[i] = loc;
+      } else {
+    	  startLocation = new Location();
+    	  endLocation = new Location();
+
+    	  startLocation.streamIn(input);
+    	  endLocation.streamIn(input);
+      }
+    }
   }
 }
