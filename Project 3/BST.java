86a098edde0b03977a5845d8488eb64e21fc7fb6
Hannah Riedman <hannah.riedman1@marist.edu>
2018-03-30 20:39:52 -0400

    fixing style and adding comments

--- Project 3/BST.java
+++ Project 3/BST.java
@@ -2,12 +2,18 @@
  * Project 3 -- Tree-based encryption and decryption
  * Filename: BST.java
  * Student name: Hannah Riedman
- * 3-1-18
+ * 3-30-18
  * includes BSTNode, BST, and EncryptionTree class
  */
 
 import java.util.Scanner;
-
+/**
+ * BSTNode class
+ * In the BSTNode class there are 3 perameters(data, left, right) for every
+ * Binary Search Tree Node. We also have functions getleft(), getRight(),
+ * getData(), printPreorder(), minNode(), maxNode(), and verifySearchOrder().
+ * This class will be used in the BST class.
+ */
 class BSTNode {
   protected  BSTNode(BSTNode t) { assert(false); }
 
@@ -22,7 +28,12 @@ class BSTNode {
   public BSTNode getLeft()  { return left;  }
   public BSTNode getRight()  { return right; }
   public String getData()    { return data;  }
-
+  /**
+   * printPreorder method
+   * This method, will recursivly print the Binary Search Tree and print NULL
+   * for every node that is NULL. There are two methods, one is overloaded with
+   * a parameter so we can print out the tree with correct indentation.
+   */
   public void printPreorder() {
     String indent = "";
     if (data == null) {
@@ -60,7 +71,6 @@ class BSTNode {
   }
 
   public BSTNode minNode() {
-    //assert(root != null);
     BSTNode node = this;
     while (node.left != null) {
       node = node.left;
@@ -69,7 +79,6 @@ class BSTNode {
   }
 
   public BSTNode maxNode() {
-    //assert(root != null);
     BSTNode node = this;
     while (node.right != null) {
       node = node.right;
@@ -89,7 +98,11 @@ class BSTNode {
     }
   }
 }
-
+/**
+ * BST Class
+ * THe BST Class holds only one parameter, the root (a BSTNode), and functions
+ * isEqual, insert(item), and remove(item).
+ */
 class BST {
   protected BST(BST t) { assert(false); }
   protected BST isEqual(BST t) { assert(false); return this; }
@@ -99,7 +112,16 @@ class BST {
   public BST() {
     root = null;
   }
-  // compareto returns a negitive if the string being compared is greater
+  /**
+   * insert method
+   * @param item is the item you want to add to the BST.
+   * The method will first ask if the root is empty so then we know we are
+   * adding a root item. If not, we will loop through comparing the item
+   * passed in, to the nodes of the tree. If we find a node that is equal to
+   * the item then we return becuase you can't have two of the same item.
+   * Otherwise we split the tree in half every loop to search for the right
+   * place to insert it.
+   */
   public void insert(String item) {
     boolean flag = true;
 
@@ -111,8 +133,7 @@ class BST {
     BSTNode curNode = this.root;
 
     while (flag) {
-      int compareItem =curNode.data.compareToIgnoreCase(item);
-      //System.out.println(curNode.data+" compareto "+item+" ="+compareItem);
+      int compareItem =curNode.data.compareTo(item);
       if (compareItem == 0) {
         flag = false;
         return; // do nothing bc you can't add two of the same item
@@ -138,18 +159,25 @@ class BST {
       }
     }
   }
+  /**
+   * remove method
+   * @param item is the item you want to remove from the BST.
+   * This method will remove in 4 different ways. If the the item is not in
+   * the tree (root is null or no item match), then it will do nothing.
+   * Once we have found the item in the BST, we will remove differently if it is
+   * a leaf node, a node with 1 child, or a node with 2 children.
+   */
   public void remove(String item) {
-    boolean flag = true;
+    boolean notFound = true;
     BSTNode parent = null;
     BSTNode curNode = this.root;
     BSTNode grandchild = null;
     if (root == null) { // if there is nothing to remove
-      return;
-      // do nothing
+      return; // do nothing
     }
-
-    while ((curNode != null)&& (flag)) {
-      int compareItem =curNode.data.compareToIgnoreCase(item);
+    // search for the node
+    while ((curNode != null)&& (notFound)) {
+      int compareItem =curNode.data.compareTo(item);
       if (compareItem < 0) { // right
         parent = curNode;
         curNode = curNode.right;
@@ -157,10 +185,10 @@ class BST {
         parent = curNode;
         curNode = curNode.left;
       } else if (compareItem == 0) {
-        flag = false;
+        notFound = false;
       }
     }
-    if (flag) {
+    if (notFound) { // if the item is not in tree.
       return;
     }
     // removing leaf node
@@ -169,15 +197,15 @@ class BST {
         root = null;
       } else if (parent.left == curNode) {
         parent.left = null;
-      } else { //if (parent.right == curNode){
+      } else {
         parent.right = null;
       }
       curNode = null;
       return;
-    }// TODO add return here and then make separete if statements
+    }
     // removing node with 1 child
     if ((curNode.left == null && curNode.right != null) ||
-               (curNode.left != null && curNode.right == null)) {
+        (curNode.left != null && curNode.right == null)) {
       if (curNode.left != null) {
         grandchild = curNode.left;
         curNode.left = null;
@@ -195,9 +223,8 @@ class BST {
       }
       curNode = null;
       return;
-      // TODO add return here and then make separete if statements
-    // removing node with 2 children
     }
+    // removing node with 2 children
     if (curNode.left != null && curNode.right != null) {
       BSTNode leftmost = curNode.right;
       BSTNode leftmostParent = curNode;
@@ -231,10 +258,22 @@ class BST {
   public void verifySearchOrder() { if (root != null) root.verifySearchOrder(); }
 
 }
-
+/**
+ * EncryptionTree Class
+ * This class is an extension of the BST class, and it also has more functions,
+ * encrypt(item), and decrypt(path). These two functions decode/encode strings
+ * using the elments in the Binary Search Tree.
+ */
 class EncryptionTree extends BST {
   public EncryptionTree() {}
-
+  /**
+   * encrypt method
+   * @param  item the item you want to encypt.
+   * @return path, the path that you will take to get to that item. If item
+   * is not in BST then '?' will be the path.
+   * This method will take in an item and search and record the path from the
+   * root to that item.
+   */
   public String encrypt(String item) {
     BSTNode curNode = root;
     boolean found = false;
@@ -243,11 +282,8 @@ class EncryptionTree extends BST {
       path = "?";
       return path;
     }
-    //System.out.println("String:"+item);
     while ((curNode != null)&& (!found)) {
-      // TODO use compareTo instead of ignorecase to save time
       int compareItem = curNode.data.compareTo(item);
-      //System.out.println(curNode.data+" compareto "+item+" ="+compareItem);
       if (compareItem == 0) {
         found = true;
       } else if (compareItem > 0) { // left path
@@ -263,13 +299,20 @@ class EncryptionTree extends BST {
     }
     return path;
   }
-
+  /**
+   * decrypt method
+   * @param  path the path is the path to an item in the tree.
+   * @return item, that is found from the path. If the path leads to no item, a
+   * '?' is returned.
+   * The method will split the tree in half every loop, following the path, '1'
+   * is right and '0' is left. 
+   */
   public String decrypt(String path) {
     BSTNode curNode = root;
     String item = "?";
     String direction = "2";
     if (root == null){
-      item = "?";  // TODO make this one line?
+      item = "?";
       return item;
     }
     for (int i=0;i<path.length();i++) {
--- Project 3/Driver_prj3.java
+++ Project 3/Driver_prj3.java
@@ -2,80 +2,77 @@
  * Project 3 -- Tree-based encryption and decryption
  * Filename: Driver_prj3.java
  * Student name: Hannah Riedman
- * 3-1-18
+ * 3-30-18
  * Tree-based encryption and decryption Driver.
  */
 import java.util.Scanner;
 
 public class Driver_prj3 {
-
   public static void main(String[] args) {
     Scanner file = new Scanner(System.in);
 
     EncryptionTree newtree = new EncryptionTree();
-    String command = "";
-    String content = "";
-    String[] content1;
-    String result = "";
-    String[] splitContents;
-    int space = 0;
-    Boolean flag = false;
+    String command = ""; // holds the first letter or "command" on each line
+    String[] content; // holds the content of a line
+    String result = ""; // holds the results of encypt/decrypt
+    String[] splitContents; // holds the messages that need to be encypt/decrypt
+    Boolean quit = false; // if the user wants to quit
     while (file.hasNext()){
       String currentLine = file.nextLine();
       command = currentLine.substring(0,1);
-      if (flag){ // if user has invoked quit
+
+      if (quit){ // if user has invoked quit
         break;
       }
 
       switch (command) {
         case "i": // insert word
-          content = currentLine.substring(2); //.trim();
-          newtree.insert(content);
+          newtree.insert(currentLine.substring(2));
           break;
         case "r": // remove word
-          content = currentLine.substring(2);
-          newtree.remove(content);
+          newtree.remove(currentLine.substring(2));
           break;
         case "e": // encrypt message
-         // TODO fix encrypt for the first word is becoming question mark
-          content1 = currentLine.split("'");
-          splitContents = content1[1].split(" ");
+          content = currentLine.split("'");
+          splitContents = content[1].split(" ");
+          // for each item, encypt it and add to result.
           for (int i=0;i < splitContents.length; i++) {
-            //System.out.println("word:"+splitContents[i]);
             String word = splitContents[i];
             if (!word.equals(" ")) {
-              if (i == splitContents.length-1) { // if its the last case it wont
-                result = result+newtree.encrypt(word); // add a space at end to
-              } else {  // increase efficency and make it easier
+              // if we have reached the last case then we don't need an extra ""
+              if (i == splitContents.length-1) {
+                result = result+newtree.encrypt(word);
+              } else {
                 result = result+newtree.encrypt(word)+" ";
               }
             }
           }
           System.out.println(result);
-          result = "";
+          result = ""; // reset the result string to empty
           break;
         case "d": // decrypt message
-          content1 = currentLine.split("'");
-          splitContents = content1[1].split(" ");
+          content = currentLine.split("'");
+          splitContents = content[1].split(" ");
+          // for each item, decrypt it and add to result.
           for (int i=0;i < splitContents.length; i++) {
-            //System.out.println("word:"+splitContents[i]);
             String code = splitContents[i];
             if (!code.equals(" ")) {
-              if (i == splitContents.length-1) { // if its the last case it wont
-                result = result+newtree.decrypt(code); // add a space at end to
-              } else {  // increase efficency and make it easier
+              // if we have reached the last case then we don't need an extra ""
+              if (i == splitContents.length-1) {
+                result = result+newtree.decrypt(code);
+              } else {
                 result = result+newtree.decrypt(code)+" ";
               }
             }
           }
           System.out.println(result);
-          result = "";
+          result = ""; // reset the result string to empty
           break;
         case "p": // print the codebook
           newtree.printPreorder();
           break;
         case "q": // quit the program
-          flag = true;
+          quit = true;
           break;
         default:
           break;
